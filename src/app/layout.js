852dee0eef26d50110c0f632a952223af5c8957a
Abdul Sami <abdulsamisiddiq2001@gmail.com>
2024-10-08 22:10:57 +0500

    basic setup

---
+++ src/app/layout.js
@@ -0,0 +1,30 @@
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata = {
+  title: "Savvy Tools Kit",
+  description: "Savvytoolskit.com - a website or platform that provides a collection of smart or well-informed tools and resources designed to help users be more effective in their tasks or projects.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Savvy Tools Kit</h1>
+      <h3>Savvytoolskit.com - a website or platform that provides a collection of smart or well-informed tools and resources designed to help users be more effective in their tasks or projects.</h3>
+    </div>
+  );
+}
